d05305e786b999b8bfd59ce1b0f51a158cf31127
KeseterG <zhiwan13579@126.com>
2023-01-17 14:20:42 +0800

    feat: add coordinator

---
+++ src/main/java/frc/robot/coordinators/Coordinator.java
@@ -0,0 +1,40 @@
+package frc.robot.coordinators;
+
+import org.frcteam6941.looper.UpdateManager.Updatable;
+
+public class Coordinator implements Updatable{
+    @Override
+    public synchronized void read(double time, double dt){
+        // Auto Generated Method
+    }
+    
+    @Override
+    public synchronized void update(double time, double dt){
+        // Auto Generated Method
+    }
+    
+    @Override
+    public synchronized void write(double time, double dt){
+        // Auto Generated Method
+    }
+    
+    @Override
+    public synchronized void telemetry(){
+        // Auto Generated Method
+    }
+    
+    @Override
+    public synchronized void start(){
+        // Auto Generated Method
+    }
+    
+    @Override
+    public synchronized void stop(){
+        // Auto Generated Method
+    }
+    
+    @Override
+    public synchronized void disabled(double time, double dt){
+        // Auto Generated Method
+    }
+}
