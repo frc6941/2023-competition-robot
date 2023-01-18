73b044401cb96839d7abd970f287540a4c46a2ce
KeseterG <zhiwan13579@126.com>
2023-01-18 14:05:34 +0800

    fix: slight changes

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -3,54 +3,75 @@ package frc.robot.subsystems;
 import org.frcteam6941.looper.UpdateManager.Updatable;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import frc.robot.Constants;
 
 public class Arm implements Updatable {
     public static class PeriodicIO {
         // INPUT
-    
+        public double armDemand = 0.0;
+        public double armFeedforwardLength = 0.0;
+
         // OUTPUT
     }
-    
+
     public PeriodicIO mPeriodicIO = new PeriodicIO();
 
-    
+    public TalonFX armMotor = new TalonFX(Constants.CANID.ARM_MOTOR);
+
     @Override
-    public synchronized void read(double time, double dt){
+    public synchronized void read(double time, double dt) {
         // Auto Generated Method
     }
-    
+
     @Override
-    public synchronized void update(double time, double dt){
+    public synchronized void update(double time, double dt) {
         // Auto Generated Method
     }
-    
+
     @Override
-    public synchronized void write(double time, double dt){
+    public synchronized void write(double time, double dt) {
         // Auto Generated Method
     }
-    
+
     @Override
-    public synchronized void telemetry(){
+    public synchronized void telemetry() {
         // Auto Generated Method
     }
-    
+
     @Override
-    public synchronized void start(){
+    public synchronized void start() {
         // Auto Generated Method
     }
-    
+
     @Override
-    public synchronized void stop(){
+    public synchronized void stop() {
         // Auto Generated Method
     }
-    
+
     @Override
-    public synchronized void disabled(double time, double dt){
+    public synchronized void disabled(double time, double dt) {
         // Auto Generated Method
     }
 
     @Override
     public void simulate() {
 
+    }
+
+    public enum STATE {
+        ANGLE,
+        PERCENTAGE,
+        HOMING,
+        OFF
+    }
+
+    private STATE state = STATE.HOMING;
+
+    public void setState(STATE state) {
+        this.state = state;
+    }
+
+    public STATE getState() {
+        return state;
     }
 }
