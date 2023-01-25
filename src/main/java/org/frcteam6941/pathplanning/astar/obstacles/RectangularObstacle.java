eeda0328f8568a8b42ad63acc1f76dccec5705bc
KeseterG <zhiwan13579@126.com>
2023-01-26 02:38:02 +0800

    feat: a star implementation

---
+++ src/main/java/org/frcteam6941/pathplanning/astar/obstacles/RectangularObstacle.java
@@ -0,0 +1,34 @@
+package org.frcteam6941.pathplanning.astar.obstacles;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class RectangularObstacle implements Obstacle{
+    private double minX = Double.POSITIVE_INFINITY;
+    private double maxX = Double.NEGATIVE_INFINITY;
+    private double minY = Double.POSITIVE_INFINITY;
+    private double maxY = Double.NEGATIVE_INFINITY;
+
+
+    public RectangularObstacle(Translation2d... cornerPoints) {
+        for(Translation2d corner:cornerPoints) {
+            if(corner.getX() < minX) {
+                minX = corner.getX();
+            }
+            if(corner.getX() > maxX) {
+                maxX = corner.getX();
+            }
+            if(corner.getY() < minY) {
+                minY = corner.getY();
+            }
+            if(corner.getY() > maxY) {
+                maxY = corner.getY();
+            }
+        }
+    }
+
+
+    @Override
+    public boolean isInObstacle(double x, double y) {
+        return x > minX && x < maxX && y > minY && y < maxY;
+    }
+}
